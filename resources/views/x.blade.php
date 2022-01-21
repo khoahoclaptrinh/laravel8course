5dd9cf6c00f56846efbbec750252b4ef1d57b627
khanhnam99 <khanhnam99@gmail.com>
2022-01-21 21:08:41 +0700

    Update layout

---
+++ resources/views/x.blade.php
@@ -0,0 +1,18 @@
+ @extends('layouts.master')
+
+ @section('content')
+ <div class="Hello">Hello home
+
+ </div>
+
+
+ @section('sidebar')
+ <div class="sidebar"> hello section </div>
+ @endsection
+
+ @section('javascript')
+ <script text="text/javascript">
+     alert('sssssssssssss')
+ </script>
+ @endsection
+ @stop
